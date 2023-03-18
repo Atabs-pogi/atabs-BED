1e2b2b08a97a79f5ec72aa9100f8830bf9e28413
MatthewRomero007 <113237170+MatthewRomero007@users.noreply.github.com>
2023-03-18 14:47:01 +0800

    Resolve

--- atabs-BED-main/src/main/java/com/atabs/atabbe/dao/EmployeeSalaryDao.java
+++ atabs-BED-main/src/main/java/com/atabs/atabbe/dao/EmployeeSalaryDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface EmployeeSalaryDao extends JpaRepository<EmployeeSalaryEntity, Long> {
 
-    @Query(value = "SELECT * FROM employee_salary WHERE eff_date = (SELECT MAX(eff_date) FROM employee_salary)", nativeQuery = true)
+    @Query(value = "SELECT s1.* FROM coop.employee_salary s1 LEFT JOIN coop.employee_salary s2 ON s1.emp_id = s2.emp_id AND s1.eff_date < s2.eff_date WHERE s2.emp_id IS NULL", nativeQuery = true)
     List <EmployeeSalaryEntity> getAllSalary();
 
     @Query(value = "SELECT * FROM employee_salary WHERE emp_id = :empId AND eff_date = (SELECT MAX(eff_date) FROM employee_salary WHERE emp_id = :empId)", nativeQuery = true)
